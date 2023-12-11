8427c76af3ca38fb4584ea645c41acf8e9c063ea
Drolt <thomas_klug@gmx.de>
2023-12-11 15:22:49 +0100

    initialize

---
+++ customerMS/src/test/java/com/carrenting/customerMS/CustomerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrenting.customerMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
